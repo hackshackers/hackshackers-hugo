a9cabc343907d436c1b366d074393f8937694493
Matt N <mattn@alleyinteractive.com>
2016-12-13 14:38:13 -0600

    setting up mobile nav

--- themes/hackshackers-2017/webpack-src/js/app/mobile-menu.js
+++ themes/hackshackers-2017/webpack-src/js/app/mobile-menu.js
@@ -1,9 +1,11 @@
 export default function () {
   const menuBtn = document.querySelector('.hamburger-menu');
+  const mainNav = document.querySelector('.main-nav');
   if (menuBtn) {
     menuBtn.addEventListener('click', (evt) => {
       evt.preventDefault();
       console.log('hello');
+      mainNav.classList.add('active');
     });
   }
 }
