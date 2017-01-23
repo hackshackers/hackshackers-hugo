b1a517d4bf611232601192265c5629e20f056f01
Josh Kadis <josh@alleyinteractive.com>
2017-01-23 10:03:19 -0500

    code style

--- themes/hackshackers-2017/webpack-src/js/map/config.js
+++ themes/hackshackers-2017/webpack-src/js/map/config.js
@@ -29,13 +29,13 @@ export default {
     className: 'group-popup',
   },
   markerOpts: {
-    iconUrl:       '/images/marker-icon-2x.png', // L.Browser.retina not totally reliable, so just use retina icon all the time
+    iconUrl: '/images/marker-icon-2x.png', // L.Browser.retina not totally reliable, so just use retina icon all the time
     iconRetinaUrl: '/images/marker-icon-2x.png',
-    shadowUrl:     '/images/marker-shadow.png',
-    iconSize:    [25, 41],
-    iconAnchor:  [12, 41],
+    shadowUrl: '/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
     popupAnchor: [1, -34],
     tooltipAnchor: [16, -28],
-    shadowSize:  [41, 41],
+    shadowSize: [41, 41],
   },
 };
