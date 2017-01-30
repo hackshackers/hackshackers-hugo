e95fee6e172c37c1192b13792a6a02aa4359117b
Matt N <mattn@alleyinteractive.com>
2017-01-30 16:50:09 -0600

    search input focus fix

--- themes/hackshackers-2017/webpack-src/js/app/search-input.js
+++ themes/hackshackers-2017/webpack-src/js/app/search-input.js
@@ -1,12 +1,12 @@
 export default function () {
   const searchBtn = document.querySelector('.main-nav-search');
   const searchOverlay = document.querySelector('.search-overlay');
-  const searchInput = document.querySelector('.input-search');
+  const searchInput = document.querySelector('.input-search-desktop');
   if (searchBtn) {
     searchBtn.addEventListener('click', (evt) => {
       evt.preventDefault();
-      searchOverlay.classList.toggle('show');
       searchInput.focus();
+      searchOverlay.classList.toggle('show');
     });
   }
 }
