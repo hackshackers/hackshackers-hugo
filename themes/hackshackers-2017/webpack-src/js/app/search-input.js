251759ce10932c378c3336989f063d9a83eb619f
Lukas Udstuen <lukas.udstuen@gmail.com>
2021-05-02 17:53:54 -0300

    add for mobile support

--- themes/hackshackers-2017/webpack-src/js/app/search-input.js
+++ themes/hackshackers-2017/webpack-src/js/app/search-input.js
@@ -2,6 +2,8 @@ export default function () {
   const searchBtn = document.querySelector('.main-nav-search');
   const searchOverlay = document.querySelector('.search-overlay');
   const searchInput = document.getElementById('input-search-desktop');
+  const fullBleed = document.querySelector('.full-bleed');
+  // const mobileNavSearch = document.querySelector('.mobile-nav-search');
   if (searchBtn) {
     searchBtn.addEventListener('click', (evt) => {
       evt.preventDefault();
@@ -11,14 +13,17 @@ export default function () {
 
     document.addEventListener('keydown', ({ key }) => {
       if (('Escape' === key || 'Enter' === key) && searchOverlay.classList.contains('show')) {
-        // If a user presses the enter or the escape key,
+        // DESKTOP If a user presses the enter or the escape key,
         // and the search toggle is open, close it.
         searchOverlay.classList.toggle('show');
       }
     });
 
-    document.addEventListener('click', (evt) => {
-      evt.preventDefault();
+    document.querySelector('.mobile-nav-search button').onclick = function () {
+      // MOBILE: Close side bar if either enter pressed, or sidebar clicked.
+      fullBleed.classList.toggle('active');
+    };
+    document.addEventListener('click', () => {
       if (searchOverlay.contains(event.target) &&
       !searchInput.contains(event.target)) {
         // Close the search toggle if clicking anywhere besides the
