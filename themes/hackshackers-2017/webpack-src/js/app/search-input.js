f645c67b59fc38e4218bf6882442dc62ae1a09dd
Lukas Udstuen <lukas.udstuen@gmail.com>
2021-05-02 16:07:42 -0300

    works on desktop

--- themes/hackshackers-2017/webpack-src/js/app/search-input.js
+++ themes/hackshackers-2017/webpack-src/js/app/search-input.js
@@ -8,5 +8,23 @@ export default function () {
       searchOverlay.classList.toggle('show');
       searchInput.focus();
     });
+
+    document.addEventListener('keydown', ({ key }) => {
+      if (('Escape' === key || 'Enter' === key) && searchOverlay.classList.contains('show')) {
+        // If a user presses the enter or the escape key,
+        // and the search toggle is open, close it.
+        searchOverlay.classList.toggle('show');
+      }
+    });
+
+    document.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      if (searchOverlay.contains(event.target) &&
+      !searchInput.contains(event.target)) {
+        // Close the search toggle if clicking anywhere besides the
+        // text box
+        searchOverlay.classList.toggle('show');
+      }
+    });
   }
 }
