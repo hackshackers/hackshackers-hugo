6afa90720e00ade93a0934921b413706ea04b7ee
Lukas Udstuen <lukas.udstuen@gmail.com>
2022-02-07 11:44:59 -0300

    add

--- themes/hackshackers-2017/webpack-src/webpack.config.js
+++ themes/hackshackers-2017/webpack-src/webpack.config.js
@@ -33,21 +33,28 @@ var webpackConfig = {
       {
         test: /\.scss$/,
         use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-        // use: [{
-        //     loader: 'css-loader',
-        //     options: { autoprefixer: false, sourceMap: false}
-        //   },
-        //   {
-        //     loader: 'postcss?parser=postcss-scss',
-        //     options: {sourceMap: true, scss: false}
-        //   }
-        // ]
+      fallback: 'style-loader',
+         use: [
+           {
+             loader: 'css-loader',
+             options: { autoprefixer: false, sourceMap: false}
+           },
+           {
+             loader: 'postcss-loader?parser=postcss-scss',
+             options: {
+                       sourceMap: true, 
+                       scss: false,
+                       }
+           },
+       //    {
+       //      loader: 'postcss-scss'
+       //    }
+         ]
         // todo: it works with just css-loader, but the page is messed up. when i add the string of css?-autoprefix.... it breaks because of missing dependency. i suspect that if i can find what is the missing dependency it will work. could there be some sort of a dependency that i am missing and didnt declare earlier in the file? 
-         // use: "css?-autoprefixer&sourceMap!postcss-loader?parser=postcss-scss!sass-loader?sourceMap"
+     // use: "css?-autoprefixer&sourceMap!postcss-loader?parser=postcss-scss!sass-loader?sourceMap"
      // use: [require.resolve('css-loader'), require.resolve('sass-loader')]
      // use: [require.resolve('css-loader'), require.resolve('sass-loader')]
-      use: [require.resolve('css-loader?-autoprefixer&sourceMap!postcss-loader?parser=postcss-scss!sass-loader?sourceMap'), require.resolve('sass-loader')]
+     //  use: [require.resolve('css-loader?-autoprefixer&sourceMap!postcss-loader?parser=postcss-scss!sass-loader?sourceMap'), require.resolve('sass-loader')]
 //          use: [
 //             {
 //             loader: 'css-loader',
